2d217c96372ff2973595d90fd9e6605b86a738d1
Diniyar <d.zhagparov@galamat.biz>
2022-11-12 12:45:42 +0600

    remove provider

--- terraform/random-api-app.tf
+++ terraform/random-api-app.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = ">= 2.0"
-    }
-  }
-}
-
 resource "digitalocean_app" "random-api" {
   spec {
     name   = "random-api"
