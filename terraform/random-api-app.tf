debbd1158d815161cc7dc42e912b200b003fa6b5
Diniyar <d.zhagparov@galamat.biz>
2022-11-14 16:56:25 +0600

    fix credentials and backends

--- terraform/random-api-app.tf
+++ terraform/random-api-app.tf
@@ -1,13 +1,15 @@
 terraform {
   backend "s3" {
-    endpoint                    = "https://terraformstatesaves.fra1.digitaloceanspaces.com"
-    key                         = "terraform.tfstate"
-    bucket                      = "rappiddev-terraform-remote-state"
-    region                      = "fra1"
-    skip_metadata_api_check     = true
+    endpoint                = "https://terraformstatesaves.fra1.digitaloceanspaces.com"
+    key                     = "terraform.tfstate"
+    bucket                  = "states"
+    region                  = "us-west-1"
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
   }
 }
-
 
 resource "digitalocean_app" "random-api" {
   spec {
